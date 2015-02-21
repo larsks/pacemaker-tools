100fffb4d3490618abe69fe7cb2bf55666f4ccb6
Lars Kellogg-Stedman <lars@redhat.com>
2015-02-21 14:43:52 -0500

    simplify graph generation

--- cibparser.py
+++ cibparser.py
@@ -38,59 +38,73 @@ def main():
     resources = {}
     resources_aux = {}
 
+    LOG.info('finding top-level resources')
     for rsrc in doc.xpath('/cib/configuration/resources/*'):
         id = rsrc.get('id')
+        LOG.debug('found top-level resource %s', id)
         resources[id] = {
             'id': id,
             'kind': rsrc.tag,
         }
+
+    LOG.info('finding second-level primitives')
     for rsrc in doc.xpath('/cib/configuration/resources/*/primitive'):
         id = rsrc.get('id')
+        LOG.debug('found second-level resource %s', id)
         resources_aux[id] = {
             'id': id,
             'kind': rsrc.tag,
         }
 
-    kinds = set(x['kind'] for x in resources.values())
+    LOG.info('calculating colors')
+    kinds = set(x['kind'] for x in
+                resources.values() +
+                resources_aux.values())
     colors = colerator.Simple(len(kinds))
     kinds = dict(zip(kinds, colors))
 
+    visible_ids = set(resources.keys())
+
+    LOG.info('finding start constraints')
     for order in doc.xpath('/cib/configuration/constraints/rsc_order'):
         if order.get('first-action') != 'start':
             continue
         if order.get('then-action') != 'start':
             continue
 
-        constraints.append((order.get('first'), order.get('then')))
+        first_id = order.get('first')
+        then_id = order.get('then')
 
-    if args.debug:
-        import pprint
-        pprint.pprint(constraints, stream=sys.stderr)
+        visible_ids.add(first_id)
+        visible_ids.add(then_id)
+        constraints.append((first_id, then_id))
 
+    LOG.info('generating graph')
     print 'digraph {'
     print 'rankdir=LR'
-    rids = {}
+
+    LOG.debug('printing nodes')
+    nodes = {}
     counter = itertools.count()
-    for resource in resources.values():
-        rids[resource['id']] = 'node%d' % counter.next()
+    for rid in visible_ids:
+        rsrc = resources.get(rid, resources_aux.get(rid))
+        if rsrc is None:
+            raise KeyError(rsrc)
+        rsrc['node'] = 'node%d' % counter.next()
+        nodes[rsrc['id']] = rsrc['node']
+        rsrc['color'] = kinds[rsrc['kind']]
+
         print '%s [label="%s", color="%s", style="filled"]' % (
-            rids[resource['id']],
-            resource['id'],
-            kinds[resource['kind']])
+            rsrc['node'],
+            rsrc['id'],
+            rsrc['color'])
 
+    LOG.debug('printing edges')
     for left, right in constraints:
-        for id in [left, right]:
-            if id in resources_aux:
-                resource = resources_aux[id]
-                rids[resource['id']] = 'node%d' % counter.next()
-                print '%s [label="%s", color="%s", style="filled"]' % (
-                    rids[resource['id']],
-                    resource['id'],
-                    kinds[resource['kind']])
-
-    for left, right in constraints:
-        print '%s -> %s' % (rids[left], rids[right])
+        print '%s -> %s' % (nodes[left], nodes[right])
     print '}'
+
+    LOG.info('all done')
 
 
 if __name__ == '__main__':
